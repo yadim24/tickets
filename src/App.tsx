c3d791a37200e77c3a987368e017d73310ea417a
Dmitriy Yakovets <dmitriy.yakovets@gmail.com>
2023-09-18 23:05:50 +0300

    feat: Фильтр style: Кастомные чек-боксы

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styles from 'App.module.css';
+import clsx from 'clsx';
 import { ReactElement } from 'react';
 
 export function App(): ReactElement {
@@ -9,8 +10,77 @@ export function App(): ReactElement {
       </header>
       <main className={styles['content-wrapper']}>
         <div className={styles['filter-wrapper']}>
-          <div className={styles['currency-wrapper']}>
-            <h2 className={styles['filter-headers']}>ВАЛЮТА</h2>
+          <h2 className={styles['filter-headers']}>ВАЛЮТА</h2>
+          <div className={styles['currency-switcher']}>
+            <button
+              className={clsx([styles.currency, styles['currency-rur']])}
+              type="button"
+            >
+              RUR
+            </button>
+            <button
+              className={clsx([styles.currency, styles['currency-usd']])}
+              type="button"
+            >
+              USD
+            </button>
+            <button
+              className={clsx([styles.currency, styles['currency-eur']])}
+              type="button"
+            >
+              EUR
+            </button>
+          </div>
+          <h2 className={styles['filter-headers']}>КОЛИЧЕСТВО ПЕРЕСАДОК</h2>
+          <div className={styles['transfer-wrapper']}>
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label className={styles['checkbox-wrapper']}>
+              Все
+              <input
+                className={styles.checkbox}
+                type="checkbox"
+                value="all"
+                defaultChecked
+              />
+              <span className={styles.checkmark} />
+            </label>
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label className={styles['checkbox-wrapper']}>
+              Без пересадок
+              <input
+                className={styles.checkbox}
+                type="checkbox"
+                value="direct"
+              />
+              <span className={styles.checkmark} />
+            </label>
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label className={styles['checkbox-wrapper']}>
+              1 пересадка
+              <input className={styles.checkbox} type="checkbox" value="one" />
+              <span className={styles.checkmark} />
+            </label>
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label className={styles['checkbox-wrapper']}>
+              2 пересадки
+              <input className={styles.checkbox} type="checkbox" value="two" />
+              <span className={styles.checkmark} />
+            </label>
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label className={styles['checkbox-wrapper']}>
+              3 пересадки
+              <input
+                className={styles.checkbox}
+                type="checkbox"
+                value="three"
+              />
+              <span className={styles.checkmark} />
+            </label>
+          </div>
+        </div>
+        <div className={styles['list-container']}>
+          <div className={styles['ticket-wrapper']}>
+            <p>Билет</p>
           </div>
         </div>
       </main>
