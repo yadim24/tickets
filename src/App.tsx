521fdcbf9fb034db3616cb4060b2ba2d919cf6bd
Dmitriy Yakovets <dmitriy.yakovets@gmail.com>
2023-09-21 00:35:55 +0300

    feat: Верстка билета

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import styles from 'App.module.css';
 import clsx from 'clsx';
+import { FlyingPlane } from 'components/FlyingPlane';
 import { ReactElement } from 'react';
 
 export function App(): ReactElement {
@@ -12,28 +14,37 @@ export function App(): ReactElement {
         <div className={styles['filter-wrapper']}>
           <h2 className={styles['filter-headers']}>ВАЛЮТА</h2>
           <div className={styles['currency-switcher']}>
-            <button
-              className={clsx([styles.currency, styles['currency-rur']])}
-              type="button"
-            >
+            <label className={clsx([styles.currency, styles['currency-rur']])}>
               RUR
-            </button>
-            <button
-              className={clsx([styles.currency, styles['currency-usd']])}
-              type="button"
-            >
+              <input
+                className={styles.radio}
+                type="radio"
+                name="currency"
+                value="rur"
+                defaultChecked
+              />
+            </label>
+            <label className={clsx([styles.currency, styles['currency-usd']])}>
               USD
-            </button>
-            <button
-              className={clsx([styles.currency, styles['currency-eur']])}
-              type="button"
-            >
+              <input
+                className={styles.radio}
+                type="radio"
+                name="currency"
+                value="usd"
+              />
+            </label>
+            <label className={clsx([styles.currency, styles['currency-eur']])}>
               EUR
-            </button>
+              <input
+                className={styles.radio}
+                type="radio"
+                name="currency"
+                value="eur"
+              />
+            </label>
           </div>
           <h2 className={styles['filter-headers']}>КОЛИЧЕСТВО ПЕРЕСАДОК</h2>
           <div className={styles['transfer-wrapper']}>
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label className={styles['checkbox-wrapper']}>
               Все
               <input
@@ -44,7 +55,6 @@ export function App(): ReactElement {
               />
               <span className={styles.checkmark} />
             </label>
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label className={styles['checkbox-wrapper']}>
               Без пересадок
               <input
@@ -54,19 +64,16 @@ export function App(): ReactElement {
               />
               <span className={styles.checkmark} />
             </label>
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label className={styles['checkbox-wrapper']}>
               1 пересадка
               <input className={styles.checkbox} type="checkbox" value="one" />
               <span className={styles.checkmark} />
             </label>
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label className={styles['checkbox-wrapper']}>
               2 пересадки
               <input className={styles.checkbox} type="checkbox" value="two" />
               <span className={styles.checkmark} />
             </label>
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label className={styles['checkbox-wrapper']}>
               3 пересадки
               <input
@@ -80,7 +87,36 @@ export function App(): ReactElement {
         </div>
         <div className={styles['list-container']}>
           <div className={styles['ticket-wrapper']}>
-            <p>Билет</p>
+            <div className={styles.carrier}>
+              <img
+                className={styles['carrier-logo']}
+                src="turkish_airlines.svg"
+                alt="Turkish Airlines logo"
+              />
+              <button className={styles.price} type="button">
+                Купить
+                <br />
+                за 21 867 р
+              </button>
+            </div>
+            <div className={styles['flight-detail-wrapper']}>
+              <div className={styles['time-wrapper']}>
+                <div className={styles.time}>10:00</div>
+                <div className={styles.transfer}>
+                  <div>1 пересадка</div>
+                  <FlyingPlane />
+                </div>
+                <div className={styles.time}>20:00</div>
+              </div>
+              <div className={styles['airport-wrapper']}>
+                <div className={styles.airport}>VVO, Владивосток</div>
+                <div className={styles.airport}>Тель-Авив, TLV</div>
+              </div>
+              <div className={styles['date-wrapper']}>
+                <div className={styles.date}>9 Окт 2018, Пт</div>
+                <div className={styles.date}>9 Окт 2018, Пт</div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
