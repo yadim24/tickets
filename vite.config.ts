ef8970965d7cb34c9dd87824b6d1cd47f9399dcc
Dmitriy Yakovets <dmitriy.yakovets@gmail.com>
2023-09-24 22:31:14 +0300

    chore: Исправлен deploy в файле vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
+    base: '/tickets/',
     plugins: [
       react(),
       tsconfigPaths(),
